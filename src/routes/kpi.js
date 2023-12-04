ba2f31f075e5625484861f47ec69dc0f4bad25c0
AlexMagar <lx.mgr07@gmail.com>
2023-12-04 22:07:32 +1100

    feat: getKpi

--- src/routes/kpi.js
+++ src/routes/kpi.js
@@ -1,10 +1,11 @@
 import express from "express"
+import { getKpi } from "../models/KPI/KpiModel.js"
 
 const router = express.Router()
 
 router.get("/kpi", async (req, res) => {
     try {
-        const kpi = await 
+        const kpi = await getKpi()
         
     } catch (error) {
         res.status(404)
