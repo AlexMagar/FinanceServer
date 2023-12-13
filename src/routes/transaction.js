5165c2e3b8f0e6f27e44f3d65201cc961eaa541f
AlexMagar <lx.mgr07@gmail.com>
2023-12-13 23:43:29 +1100

    feat: franctons

---
+++ src/routes/transaction.js
@@ -0,0 +1,21 @@
+import express from "express"
+import { getKpi } from "../models/KPI/KpiModel.js"
+
+const router = express.Router()
+
+router.get("/transactions", async (req, res) => {
+    try {
+        const kpis = await getKpi()
+
+        res.status(200).json(kpis)
+        
+    } catch (error) {
+        res.status(404)
+        .json({
+            status: "Error",
+            message: error.message
+        })
+    }
+}) 
+
+export default router
