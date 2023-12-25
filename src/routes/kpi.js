7c981072efff4a598fd4fc02b559144d41ea3782
AlexMagar <lx.mgr07@gmail.com>
2023-12-25 22:41:53 +1100

    feat: findKpi and insertKpi

--- src/routes/kpi.js
+++ src/routes/kpi.js
@@ -1,11 +1,13 @@
 import express from "express"
-import KPI from "../models/KPI.js"
+import { findKPI } from "../models/KPIModel.js"
+import { kpis, products, transactions } from "../data/data.js";
 
 const router = express.Router()
 
 router.get("/kpi", async (req, res) => {
     try {
-        const kpis = await KPI.find()
+        const kpis = await findKPI();
+        const kpiData = await insertKpi(kpis)
 
         res.status(200).json(kpis)
         
