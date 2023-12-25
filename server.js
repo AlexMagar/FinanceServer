a0d62da8037a741bcced1731e92aff4efec3a02e
AlexMagar <lx.mgr07@gmail.com>
2023-12-25 22:12:51 +1100

    chore: db connection and server connection

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import connectMongoDB from "./src/config/mongoConfig.js";
 import kpiRoutes from "./src/routes/kpi.js";
 import transactionRoutes from "./src/routes/transaction.js";
 import productRoutes from "./src/routes/product.js";
 import { kpis, products, transactions} from "./src/data/data.js"
+import mongoose from "mongoose";
+import KPI from "./src/models/KPI.js";
+import Product from "./src/models/Product.js";
+import Transaction from "./src/models/Transaction.js";
 
 dotenv.config()
 const PORT = process.env.PORT || 8000
 const app = express()
-
-//connet db
-connectMongoDB()
 
 //middleware
 app.use(express.json()) //to parse the json data
@@ -33,11 +33,18 @@ app.use("/kpi", kpiRoutes)
 app.use("/product", productRoutes)
 app.use("/transaction", transactionRoutes)
 
-app.listen(PORT, (err) => {
-    err
-    ? console.log(err.message)
-    : console.log(`server running at http://localhost:${PORT}`)
-    //   Add data one time only or as needed
-    // transactionRoutes.insertMany(transactions)
-    // await mongoose.Connection.db.dropDatabase()
+
+// mongoose setup
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+.then ( async () => {
+    app.listen(PORT, () => console.log(`server Port: ${PORT}`))
+    // add data one time only or as needed
+    // await mongoose.connection.db.dropDatabase()
+    // KPI.insertMany(kpis)
+    // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
+})
+.catch((error) => console.log(`${error} did not connect`))
