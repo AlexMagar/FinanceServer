878e082a82a255d80f599d4c49cb8d557c99d1f5
AlexMagar <lx.mgr07@gmail.com>
2023-12-27 21:38:21 +1100

    feat: post to add new transaction

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,15 +1,33 @@
 import express from "express"
-import Transaction from "../models/Transaction.js"
+import { addTransaction, findTransaction } from "../models/TransactionModel.js"
+import { transactions } from "../data/data.js"
 
 const router = express.Router()
 
+router.post("/transaction", async ( req, res) => {
+    try {
+        const rtl = addTransaction(transactions)
+        rtl?._id
+        ? res.status(200).json({
+            message: "New Transaction has been added"
+        })
+        : res.status(400).json({
+            message: "Error, Unable to add new Transaction"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+})
+
 router.get("/transaction", async (req, res) => {
     try {
-        const transactions = await Transaction.find()
+        const trn = findTransaction()
             .limit(50)
             .sort({createdOn: -1})
 
-        res.status(200).json(transactions)
+        res.status(200).json(trn)
         
     } catch (error) {
         res.status(404)
