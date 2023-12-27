d991e19f8d28204593bc570872eb2bf5eea0f56b
AlexMagar <lx.mgr07@gmail.com>
2023-12-27 21:37:48 +1100

    feat: post method to add products

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,34 @@
 import express from "express"
-import products from "../models/Product.js"
+import { products } from "../data/data.js";
+import { addProduct, findProduct } from "../models/ProductModel.js";
 
 const router = express.Router()
 
+router.post("/product", async (req, res) => {
+    try {
+        const rslt = addProduct(products)
+
+        rslt?._id 
+        ? res.status(200).json({
+            message: "New Product has been added"
+        })
+        : res.status(400).json({
+            message: "Error, unable to add products"
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+})
+
 router.get("/product", async (req, res) => {
     try {
-        const product = await products.find()
+        const pdt = findProduct()
+
+        pdt &&
         res.status(200).json({
-            message: product
+            message: pdt
         })
     } catch (error) {
         res.status(404).json({
