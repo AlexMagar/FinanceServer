11fcb8db35906ccdb770ad5a93c1a0af816567ea
AlexMagar <lx.mgr07@gmail.com>
2023-12-29 22:04:31 +1100

    feat: kpiRouter path

---
+++ src/routes/kpiRouter.js
@@ -0,0 +1,40 @@
+import expres from 'express'
+import { addKpi, getKpi } from '../models/KPIModel.js'
+import { kpis } from '../data/data.js'
+
+const router = expres.Router()
+
+router.post("/", async (req, res) => {
+    try {
+        const interKpi = await addKpi(kpis)
+
+        interKpi?._id
+        ? res.status(200).json({
+            message: "Successfully added kpis data"
+        })
+        : res.status(400).json({
+            message: "Error on adding kpis data"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const result = await getKpi()
+        result &&
+        res.json({
+            status: "success",
+            message: "Data connected successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+export default router
